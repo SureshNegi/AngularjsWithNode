75e36e04bb675f9104d6107f928a9223211d176e
Negi <suresh-negi@mercer.com>
2017-05-30 16:36:36 +0530

    Add existing file

--- App/js/controllers.js
+++ App/js/controllers.js
@@ -1,50 +1,70 @@
 angular.module('F1FeederApp.controllers', []).
-   
-  /* Drivers controller */
-  controller('driversController', function($scope, ergastAPIservice) {
-    $scope.nameFilter = null;
-    $scope.driversList = [];
-    $scope.searchFilter = function (driver) {
-        var re = new RegExp($scope.nameFilter, 'i');
-        return !$scope.nameFilter || re.test(driver.Driver.givenName) || re.test(driver.Driver.familyName);
-    };
 
-    ergastAPIservice.getDrivers().success(function (response) {
-        //Digging into the response to get the relevant data
-        $scope.driversList = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
-    });
+  /* Drivers controller */
+  controller('driversController', function ($scope, ergastAPIservice) {
+      $scope.nameFilter = null;
+      $scope.driversList = [];
+      $scope.searchFilter = function (driver) {
+          var re = new RegExp($scope.nameFilter, 'i');
+          return !$scope.nameFilter || re.test(driver.Driver.givenName) || re.test(driver.Driver.familyName);
+      };
+
+      ergastAPIservice.getDrivers().success(function (response) {
+          //Digging into the response to get the relevant data
+          $scope.driversList = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+      });
   }).
 
   /* Driver controller */
-  controller('driverController', function($scope, $routeParams, ergastAPIservice) {
-    $scope.id = $routeParams.id;
-    $scope.races = [];
-    $scope.driver = null;
+  controller('driverController', function ($scope, $routeParams, ergastAPIservice) {
+      $scope.id = $routeParams.id;
+      $scope.races = [];
+      $scope.driver = null;
 
-    ergastAPIservice.getDriverDetails($scope.id).success(function (response) {
-        $scope.driver = response.MRData.StandingsTable.StandingsLists[0].DriverStandings[0]; 
-    });
+      ergastAPIservice.getDriverDetails($scope.id).success(function (response) {
+          $scope.driver = response.MRData.StandingsTable.StandingsLists[0].DriverStandings[0];
+      });
 
-    ergastAPIservice.getDriverRaces($scope.id).success(function (response) {
-        $scope.races = response.MRData.RaceTable.Races; 
-    }); 
+      ergastAPIservice.getDriverRaces($scope.id).success(function (response) {
+          $scope.races = response.MRData.RaceTable.Races;
+      });
   }).
 
   controller('loginController', function ($scope, $routeParams, ergastAPIservice, $location) {
-    
-        $scope.id = $routeParams.id;
-        $scope.races = [];
-        $scope.driver = null;
-       
-        $scope.login=function(){           
-            $location.path('/drivers');
-        }
-        $scope.SignUp = function () {
-            $location.path('/register');
-        }       
+
+      $scope.id = $routeParams.id;
+      $scope.races = [];
+      $scope.driver = null;
+
+      $scope.login = function () {
+          $location.path('/drivers');
+      }
+      $scope.SignUp = function () {
+          $location.path('/register');
+      }
   })
-.controller('registerController', function ($scope, $routeParams, ergastAPIservice, $location) {   
+.controller('registerController', function ($scope, $routeParams, ergastAPIservice, $location) {
     this.register = function () {
-        alert('hi');
+    
+        user_info = { fName: this.user.firstName, lName: this.user.lastName, uName: this.user.username, eMail: this.user.uEmail, passWord: this.user.password };
+    
+
+        $.ajax({
+            url: 'http://127.0.0.1:8080/addUser',
+            data: JSON.stringify({ "info": user_info }),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            type: 'POST',
+           
+            success: function (data) {
+
+                alert('yes');
+                console.log('success');
+            },
+            error: function (xhr, status, error) {
+
+                alert('no');
+            }
+        });;
     }
 });
--- server.js
+++ server.js
@@ -8,6 +8,30 @@ app.get('/', function (req, res) {
     console.log("server request");
     res.redirect('/');
 });
+app.post('/addUser', function (req, res) { 
+    var arr = [], fileName = "file.json";
+    fs = require('fs');
+    var obj = fs.readFileSync(fileName, 'utf8');
+    console.log("length=="+obj.length);
+    if (obj.length<10) {
+        console.log("no data");
+    }
+    else {
+        arr =JSON.parse(obj);    
+    }
+    var body = '';
+    filePath = 'helloworld.json';
+    req.on('data', function (data) {
+       body += data;      
+    });
 
+    req.on('end', function () {
+        arr[arr.length] = body;
+        fs.writeFile('file.json',JSON.stringify(arr), 'utf8'); 
+        var str=JSON.stringify(arr);
+        //res.end('{"msg": "OK"}');
+        res.end(str);
+    });
+});
 app.listen(8080, 'localhost');
 console.log("MyProject Server is Listening on port 8080");
